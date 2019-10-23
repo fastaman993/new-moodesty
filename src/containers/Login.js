78dc540472c80134fc6fdd45fe5c3fbd72907a5a
Adhy Fastabiqul Khoirot <fastaman993@Adhys-MacBook-Pro.local>
2019-10-24 00:08:49 +0700

    initial commit

---
+++ src/containers/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import girl from "../assets/girl.jpg";
+
+export default function Login() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh"
+      }}
+    >
+      <div
+        style={{
+          width: 500,
+          height: 300,
+          background: "white",
+          borderRadius: 20
+        }}
+      >
+        <Row>
+          <Col md={5}>
+            <img
+              src={girl}
+              style={{
+                height: 300,
+                width: 225,
+                borderTopLeftRadius: 20,
+                borderBottomLeftRadius: 20
+              }}
+            />
+          </Col>
+          <Col
+            md={7}
+            style={{
+              padding: "50px 20px 0 40px",
+              background: "#FAFAFA",
+              borderTopRightRadius: 20,
+              borderBottomRightRadius: 20
+            }}
+          >
+            <h1 style={{ color: "#3F3F3F" }}>Hi there...</h1>
+            <p style={{ color: "#929292" }}>
+              In order to use <b>Moodesty</b> you should login to spotify first
+            </p>
+            <Button
+              variant="outline-info"
+              size="lg"
+              style={{ marginTop: 20, borderRadius: 20 }}
+            >
+              <i class="fab fa-spotify"></i> Login with spotify
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
